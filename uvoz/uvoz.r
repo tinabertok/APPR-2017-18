54e6cc0aaa56257654d288a03c66848cbfeb682b
Tina Bertok <tina.bertok@gmail.com>
2017-12-14 10:09:24 +0100

    Zacela z tidy data.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -2,53 +2,35 @@
 
 sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
+
+# Funkcija, ki uvozi podatke iz datoteke istospolne_partnerske_skupnosti.csv
+uvozi.istospolne <- function() {
+  data <- read_csv2("podatki/Istospolne_partnerske_skupnosti.csv", skip = 2,
+                    locale = locale(encoding = "Windows-1250"), n_max = 2) %>%
+    melt()
+  colnames(data) <- c("spol", "leto", "stevilo")
+  data$leto <- parse_number(data$leto)
   return(data)
 }
 
 # Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
+istospolne <- uvozi.istospolne()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+
+# Funkcija, ki uvozi podatke iz datoteke Poroke_po_mesecih.csv
+uvozi.meseci <- function() {
+  data <- read_csv2("podatki/Poroke_po_mesecih.csv", skip = 2,
+                    locale = locale(encoding = "Windows-1250"), n_max = 32) %>%
+    melt(id.vars = 1)
+  colnames(data) <- c("leto", "mesec", "stevilo")
+#  data$leto <- parse_number(data$leto)
+  return(data)
+}
+
+# Zapišimo podatke v razpredelnico obcine
+meseci <- uvozi.meseci()
+
+
+
